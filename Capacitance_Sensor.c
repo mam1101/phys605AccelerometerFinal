e2f8cfda8ce23e118a95739432c462c3db9a21a3
Max Miller <mam1101@wildcats.unh.edu>
2018-05-12 21:07:15 -0400

    Create Capacitance_Sensor.c

---
+++ Capacitance_Sensor.c
@@ -0,0 +1,37 @@
+#include <CapacitiveSensor.h>
+
+/*
+ * CapitiveSense Library Demo Sketch
+ * Paul Badger 2008
+ * Uses a high value resistor e.g. 10 megohm between send pin and receive pin
+ * Resistor effects sensitivity, experiment with values, 50 kilohm - 50 megohm. Larger resistor values yield larger sensor values.
+ * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
+ * Best results are obtained if sensor foil and wire is covered with an insulator such as paper or plastic sheet
+ */
+
+/* we always wait a bit between updates of the display */
+unsigned long delaytime=500;
+unsigned long averaged_sample_max = 20;
+
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
+int ledPin = 9;
+
+int readings[numReadings];      // the readings from the analog input
+
+void setup()                    
+{
+   pinMode(ledPin, OUTPUT);
+   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(9600);
+}
+
+void loop()                    
+{
+    long start = millis();
+    long total1 =  cs_4_2.capacitiveSensor(30);
+    Serial.print(total1);
+    Serial.print("\n");
+    
+    delay(delaytime);                             // arbitrary delay to limit data to serial port 
+    
+}
